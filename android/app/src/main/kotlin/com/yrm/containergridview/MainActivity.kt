1834634fc41ef7da625afc61e167d4142b82f568
YordyM <yordi.romero22@itca.edu.sv>
2023-10-23 22:46:27 -0600

    subiendo

---
+++ android/app/src/main/kotlin/com/yrm/containergridview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yrm.containergridview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:containergridview/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Home());
